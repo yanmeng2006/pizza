cfd7562dd5320c876d9c4c824adf1b17c7c0f598
yanmeng2006 <3138821687@qq.com>
2025-03-08 00:18:27 +0800

    commit

--- src/pizza.java
+++ src/pizza.java
@@ -1,6 +1,11 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+import javax.swing.text.DateFormatter;
 
 public class pizza{
     public String storeName;
@@ -13,9 +18,14 @@ public class pizza{
     public String Ingredient2; 
     public String Ingredient3; 
     public int pizzaPrice;  
-    private String sides;
+    public String sides;
+
     private String drinks;
+    private String size;
+    private String extracheese;
+    private String sidedish;
     private String orderID;
+
     private double orderTotal;
 
     public final String DEF_ORDER_ID="DEF-SOH-099";
@@ -36,24 +46,28 @@ public class pizza{
     public StringBuilder specialPrice;
 
     public void processCardPayment(long cardNumber,String expiryDate,int cvv){
+        Scanner input = new Scanner(System.in);
 
         cardLength=Long.toString(cardNumber).length();
         if (cardLength==14) {
+            while (Long.toString(cardNumber).equals(blacklistedNumber)){
+                System.out.println("Card is blacklisted. Please use another card");
+                cardNumber=input.nextLong();
+                cardLength=Long.toString(cardNumber).length();
+            }
             System.out.println("Card accepted");
         } else{
             System.out.println("Card declined");
         }
 
-        
-        if (Long.toString(cardNumber).equals(blacklistedNumber)){
-            System.out.println("Card is blacklisted. Please use another card");
-        }else{
-            System.out.println("Card accepted");
-        }
         firstCardDigit=Integer.parseInt(Long.toString(cardNumber).substring(0, 1));
         lastFourDigit=Integer.parseInt(Long.toString(cardNumber).substring(10, 14));
         cardNumberToDisplay=firstCardDigit+"*********"+lastFourDigit;
         System.out.println("Card number is:"+ cardNumberToDisplay);
+
+        orderID=cardNumberToDisplay;
+
+        input.close();
     }
 
     public void specialOfTheDay(StringBuilder pizzaOfTheDay,StringBuilder sideOfTheDay,StringBuilder specialPrice){
@@ -104,30 +118,185 @@ public class pizza{
     }
 
     public void takeorder(){
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter three ingredients for your pizza (use spaces to separate ingredients):");
-        String Ingredient1=input.next();
-        String Ingredient2=input.next();
-        String Ingredient3=input.next();
+        System.out.println("Please pick any three of the following ingredients:\r\n" + //
+                        " 1. Mushroom\r\n" + //
+                        " 2. Paprika\r\n" + //
+                        " 3. Sun-dried tomatoes\r\n" + //
+                        " 4. Chicken\r\n" + //
+                        " 5. Pineapple\r\n" + //
+                        " Enter any three choices (1, 2, 3,…) separated by spaces:");
+        int ingChoice1=input.nextInt();
+        int ingChoice2=input.nextInt();
+        int ingChoice3=input.nextInt();
+
+        while(ingChoice1<1||ingChoice1>5||ingChoice2<1||ingChoice2>5||ingChoice3<1||ingChoice3>5){
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            ingChoice1=input.nextInt();
+            ingChoice2=input.nextInt();
+            ingChoice3=input.nextInt();
+        }
+        switch (ingChoice1) {
+            case 1:
+                Ingredient1="Mushroom";
+                break;
+             case 2:
+                Ingredient1="Paprike";
+                break; 
+            case 3:
+                Ingredient1="Sun-dried tomatoes";
+                break; 
+            case 4:
+                Ingredient1="Chicken";
+                break; 
+            case 5:
+                Ingredient1="Pineapple";
+                break;
+        
+            default:
+                break;
+        }
+        switch (ingChoice2) {
+            case 1:
+                Ingredient2="Mushroom";
+                break;
+             case 2:
+                Ingredient2="Paprike";
+                break; 
+            case 3:
+                Ingredient2="Sun-dried tomatoes";
+                break; 
+            case 4:
+                Ingredient2="Chicken";
+                break; 
+            case 5:
+                Ingredient2="Pineapple";
+                break;
+        
+            default:
+                break;
+        }
+        switch (ingChoice3) {
+            case 1:
+                Ingredient3="Mushroom";
+                break;
+             case 2:
+                Ingredient3="Paprike";
+                break; 
+            case 3:
+                Ingredient3="Sun-dried tomatoes";
+                break; 
+            case 4:
+                Ingredient3="Chicken";
+                break; 
+            case 5:
+                Ingredient3="Pineapple";
+                break;
+        
+            default:
+                break;
+        }
+
         
         
-        System.out.println("Enter size of pizza (Small, Medium, Large):");
-        String size=input.nextLine();
+        System.out.println("”What size should your pizza be?\r\n" + //
+                        " 1. Large\r\n" + //
+                        " 2. Medium\r\n" + //
+                        " 3. Small\r\n" + //
+                        " Enter only one choice (1, 2, or 3):");
+        int sizechoice=input.nextInt();
         input.nextLine();
+        while(sizechoice<1||sizechoice>3){
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            sizechoice=input.nextInt();
+        }
+        switch (sizechoice) {
+            case 1:
+                size="Large";
+                break;
+            case 2:
+                size="Medium";
+                break;
+            case 3:
+                size="Small";
+                break;
+        
+            default:
+                break;
+        }
 
         System.out.println("Do you want extra cheese (Y/N):");
-        String extracheese=input.nextLine();
+        extracheese=input.nextLine();
+        input.nextLine();
 
-        System.out.println("Enter one side dish (Calzone, Garlic bread, None):");
-        String sidedish=input.nextLine();
+        System.out.println("Following are the side dish that go well with your pizza:\r\n" + //
+                        " 1. Calzone\r\n" + //
+                        " 2. Garlic bread\r\n" + //
+                        " 3. Chicken puff\r\n" + //
+                        " 4. Muffin\r\n" + //
+                        " 5. Nothing for me\r\n" + //
+                        " What would you like? Pick one (1, 2, 3,…):");
+        int sidedishchoice=input.nextInt();
+        while (sidedishchoice<1||sidedishchoice>5) {
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            sidedishchoice=input.nextInt();
+            
+        }
+        switch (sidedishchoice) {
+            case 1:
+                sidedish="Calzone";
+                break; 
+            case 2:
+                sidedish="Garlic bread";
+                break; 
+            case 3:
+                sidedish="chicken puff";
+                break; 
+            case 4:
+                sidedish="Muffin";
+                break;
+            case 5:
+                sidedish="Nothing for me";
+                break;
         
-        System.out.println("Enter drinks(Cold Coffee, Cocoa drink, Coke, None):");
-        String drinks=input.nextLine();
+            default:
+                break;
+        }
+        
+        System.out.println("Choose from one of the drinks below. We recommend Coca Cola:\r\n" + //
+                        " 1. Coca Cola\r\n" + //
+                        " 2. Cold coffee\r\n" + //
+                        " 3. Cocoa Drink\r\n" + //
+                        " 4. No drinks for me\r\n" + //
+                        " Enter your choice:");
+        int drinkchioce=input.nextInt();
+        while(drinkchioce<1||drinkchioce>4){
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            drinkchioce=input.nextInt();
+        }
+        switch (sizechoice) {
+            case 1:
+                drinks="Coca Cola";
+                break;
+            case 2:
+                drinks="Cold coffee";
+                break;
+            case 3:
+                drinks="Cocoa Drink";
+                break;
+            case 4:
+                drinks="No drinks for me";
+                break;
+        
+            default:
+                break;
+        }
+        input.nextLine();
 
         System.out.println("Would you like the chance to pay only half for your order? (Y/N):");
         String  wantDiscount=input.nextLine();
-
         if(wantDiscount.equals("Y")||wantDiscount.equals("y")){
             isItYourBirthday();
         }else{
@@ -135,7 +304,7 @@ public class pizza{
         }
 
         makePizza();
-        printReceipt(size,extracheese,sidedish,drinks);
+        System.out.println(toString());
 
         input.close();
     }
@@ -143,12 +312,8 @@ public class pizza{
         System.out.println("Making pizza with"+ pizzaIngredients);
 
     }
-    private void printReceipt(String size,String extracheese,String sidedish,String drinks){
-        orderID=cardNumberToDisplay;
-        System.out.println("***********RECEIPT***********");
-        System.out.println("OrderID:"+ orderID);
-        System.out.println("Details:"+size+","+sidedish+","+drinks);
-        System.out.println("Order Total:"+ orderTotal);
+    public String toString(){
+        return "***********RECEIPT***********\nOrderID:"+ orderID+"\nSize:"+size+"\nextracheese:"+extracheese+"\nSidedish:"+sidedish+"\nDrinks:"+drinks+"\nOrder Total:"+ orderTotal;
     }
 
     public void isItYourBirthday(){
@@ -163,28 +328,57 @@ public class pizza{
         LocalDate now=LocalDate.now();
 
         Period diff= Period.between(dob,now);
+        while(diff.getYears()<5||diff.getYears()>120){
+            System.out.println("Invalid date. You are either too young or too dead to order. \r\n" + //
+                                "Please enter a valid date:");
+            date=input.nextLine();
+            year=Integer.parseInt(date.substring(0,4));
+            month=Integer.parseInt(date.substring(5,7));
+            day=Integer.parseInt(date.substring(8,10));
+    
+            dob=LocalDate.of(year,month,day);
+            diff= Period.between(dob,now);
+        }
         if(dob.getDayOfMonth()==now.getDayOfMonth()&&dob.getMonthValue()==now.getMonthValue()&&diff.getYears()<18){
             System.out.println("Congratulations! You pay only half the price for your order");
         }else{
             System.out.println("Too bad! You do not meet the conditions to get our 50% discountr");
         }
+        input.close();
     }
 
 
         private void makeCardPayment(){
             Scanner input=new Scanner(System.in);
-
-            System.out.println("Card number:");
-            Long cardNumber=input.nextLong();
+            System.out.println("Card Number:");
+            cardNumber=input.nextLong();
             System.out.println("Expiry Date(year-month):");
             expiryDate=input.nextLine();
             input.nextLine();
+            boolean vaildExpirydate=false;
+            while (vaildExpirydate) {
+                try{
+                    int year=Integer.parseInt(expiryDate.substring(0,4));
+                    int month=Integer.parseInt(expiryDate.substring(5, 7));
+                    LocalDate expiry=LocalDate.of(year, month,01);
+                    LocalDate now=LocalDate.now();
+                    if(expiry.isBefore(now)){
+                        System.out.println("Invalid format, Please enter the expiry date in Y-mM format:");
+                        expiryDate=input.nextLine();
+                    }else{
+                        vaildExpirydate=true;
+                    }
+                    
+
+                }catch(Exception e){
+                    System.out.println("Invalid format, Please enter the expiry date in Y-mM format:");
+                    expiryDate=input.nextLine();
+                }
+            }
+            
             System.out.println("Cvv Number:");
             int cvv=input.nextInt();
             processCardPayment(cardNumber,expiryDate,cvv);
             input.close();
-           
-
-
-    }
+        }
 }
